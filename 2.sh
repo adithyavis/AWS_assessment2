e1ffec971eb41cf14b1a0ff5bf3ae64fe645a7dd
adithyavis <40817254+adithyavis@users.noreply.github.com>
2018-08-04 23:55:27 +0530

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -8,13 +8,14 @@ subnet_id="subnet-de0385f2"
 subnet_id="subnet-de0385f2"
 aws ec2 run-instances --iam-instance-profile Name=FullAccess --image-id ami-b70554c8 --count 1 --subnet-id $subnet_id --instance-type t2.micro --key-name  $keypair --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=shell1_ec2_Adithya}]" --region us-east-1
 aws ec2 run-instances --iam-instance-profile Name=FullAccess --image-id ami-b70554c8 --count 1 --subnet-id $subnet_id --instance-type t2.micro --key-name  $keypair --subnet-id $subnet_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=shell2_ec2_Adithya}]" --region us-east-1
+echo "Sleeping for 2 minutes for instances to get initialized"
+sleep 2m
 instanceid1=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Name,Values=shell1_ec2_Adithya" | grep InstanceId | grep -E -o "i\-[0-9A-Za-z]+")
 instanceid2=$(aws ec2 describe-instances --region us-east-1 --filters "Name=tag:Name,Values=shell2_ec2_Adithya" | grep InstanceId | grep -E -o "i\-[0-9A-Za-z]+")
 ip1=$(aws ec2 describe-instances --instance-ids $instanceid1 --region us-east-1 | grep PublicIpAddress | awk -F ":" '{print $2}' | sed 's/[",]//g')
 ip2=$(aws ec2 describe-instances --instance-ids $instanceid2 --region us-east-1 | grep PublicIpAddress | awk -F ":" '{print $2}' | sed 's/[",]//g')
 ip1=$(echo "${ip1// /}")
 ip2=$(echo "${ip2// /}")
-
 
 echo "Generating public keys in both the instances"
 ssh -o CheckHostIP=no -i `echo $file` ec2-user@`echo $ip1` 'bash -s'<<'ENDSSH' 
